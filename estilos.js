6ca4453261e6737b921a77035f377a8071176c82
Guilherme Santana <g.santossantana22@gmail.com>
2022-11-04 11:59:47 -0300

    primeiros passos react-native

---
+++ estilos.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#3892F0",
+  },
+  um: {
+    flex: 2,
+    backgroundColor: "#2DF8F6",
+    width: "100%",
+    paddingTop: 20,
+  },
+  dois: {
+    backgroundColor: "#274ED9",
+    flex: 3,
+    width: "100%",
+  },
+  titulo: {
+    color: "black",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  subtitulo: {
+    color: "white",
+    fontSize: 24,
+    textAlign: "center",
+  },
+});
